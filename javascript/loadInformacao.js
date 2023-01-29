294528b5fc03be262ebb635414643009e052e4cd
Ekoko Clesh <36325257+Gabriel-1999@users.noreply.github.com>
2023-01-30 00:41:25 +0200

    Delete javascript directory

--- javascript/loadInformacao.js
+++
@@ -1,6 +0,0 @@
-var btn1 = document.getElementById("botao1");
-
-btn1.addEventListener("click", ()=>{
-    sessionStorage.setItem("link", document.getElementById("link1").value);
-    sessionStorage.setItem("descricao", document.getElementById("descricao1").value);
-})
